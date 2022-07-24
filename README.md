b6eba867c9f21c67296a7bac2d8d00ea2c2fed9e
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2022-07-24 21:27:37 +0200

    First 90 pages, automate download

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bw-covid-telegram
+
+## Telegram Public Channel Data
+
+Online Telegram Channel Export: [Telegram API Server](https://tg.i-c-a.su)
+
+RegierungBW: [RegierungBW - JSON](https://tg.i-c-a.su/json/RegierungBW)
---
+++ run-download.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+# curl -v https://tg.i-c-a.su/json/RegierungBW/2 > page-2.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/3 > page-3.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/4 > page-4.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/5 > page-5.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/6 > page-6.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/7 > page-7.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/8 > page-8.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/9 > page-9.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/10 > page-10.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/11 > page-11.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/12 > page-12.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/13 > page-13.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/14 > page-14.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/15 > page-15.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/16 > page-16.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/17 > page-17.json
+# curl -v https://tg.i-c-a.su/json/RegierungBW/18 > page-18.json
+
+for i in {31..90}
+do
+  curl -v https://tg.i-c-a.su/json/RegierungBW/$i > pagedata/page-$i.json
+  sleep 5
+done
